12b416e8d52ee02cc3ed13aa0b20d1d7dd668577
Sourcery AI <>
2023-06-23 09:56:18 +0000

    'Refactored by Sourcery'

--- upload.py
+++ upload.py
@@ -6,11 +6,13 @@ import sys
 KEY, SECRET, ENDPOINT, BUCKET, FILEPATH, FILENAME = [sys.argv[i+1] for i in range(6)]
 
 session = boto3.session.Session()
-client = session.client('s3',
-                        region_name='nyc3',
-                        endpoint_url='https://' + ENDPOINT,
-                        aws_access_key_id = KEY,
-                        aws_secret_access_key = SECRET)
+client = session.client(
+    's3',
+    region_name='nyc3',
+    endpoint_url=f'https://{ENDPOINT}',
+    aws_access_key_id=KEY,
+    aws_secret_access_key=SECRET,
+)
 
 client.upload_file(FILEPATH, # Path to local file
                    BUCKET,   # Name of Space
